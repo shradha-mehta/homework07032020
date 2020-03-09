c00fa708b369a94e201b1468919dec92299d41f7
shradha-mehta <shradhajiya@gmail.com>
2020-03-09 21:27:12 +0000

    homework07032020

---
+++ Exercise2.java
@@ -0,0 +1,20 @@
+package homework07032020;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        System.out.println(list_Strings);
+
+
+    }
+}
+
+
